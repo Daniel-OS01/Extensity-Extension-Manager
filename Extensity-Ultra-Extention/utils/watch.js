233df48bb1302d4b0718beaa1bd66f226652d3f7
Daniel-OS01 <daniel.psytech@gmail.com>
2026-03-28 17:15:33 +0300

    Remove Extensity Ultra reference workspace

--- Extensity-Ultra-Extention/utils/watch.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig, loadEnv } from 'vite'
-import { getViteConfig } from '../vite.config.js'
-
-// export default defineConfig(config);
-export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') };
-  return defineConfig(getViteConfig());
-}
